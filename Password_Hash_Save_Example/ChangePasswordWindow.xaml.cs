c298b023e80fca3dd81bde4c205a33fb700412ce
ParkEuiKyu <euikyu0423@gmail.com>
2019-11-26 12:01:41 +0900

    비밀번호 확인 및 변경 예제

---
+++ Password_Hash_Save_Example/ChangePasswordWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Password_Hash_Save_Example
+{
+    /// <summary>
+    /// ChangePasswordWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class ChangePasswordWindow : Window
+    {
+        private readonly string SALT;
+        public ChangePasswordWindow(string salt)
+        {
+            InitializeComponent();
+            //load main salt
+            this.SALT = salt;
+        }
+
+        //confirm changing password
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var id = id_TextBox.Text;
+            var passwd = currentPasswd_PasswordBox.Password;
+            if (CheckID(id) && CheckPassword(SALT, passwd))
+            {
+                var changePasswd = changePasswd_PasswordBox.Password;
+                var confirm = confirmPasswd_PasswordBox.Password;
+                if(confirm == changePasswd)
+                {
+                    ChangePassword(SALT, changePasswd);
+                    MessageBox.Show("Change password successfully.");
+                    Close();
+                }
+                else
+                {
+                    MessageBox.Show("Different password between changing one and confirm.");
+                }
+            }
+            else
+            {
+                var msg = CheckID(id) ? "Wrong password." : "Wrong ID.";
+                MessageBox.Show(msg);
+            }
+
+        }
+
+
+        private bool CheckID(string id)
+        {
+            return (Properties.Settings.Default.id == id);
+        }
+        private bool CheckPassword(string salt, string password)
+        {
+            var hash = Encoding.ASCII.GetBytes(salt + password);
+            var sha1 = new SHA1CryptoServiceProvider();
+            var sha1hash = sha1.ComputeHash(hash);
+            var hashedPassword = Encoding.ASCII.GetString(sha1hash);
+
+            return (Properties.Settings.Default.passwd == hashedPassword);
+        }
+        private void ChangePassword(string salt, string password)
+        {
+            var hash = Encoding.ASCII.GetBytes(SALT + password);
+            var sha1 = new SHA1CryptoServiceProvider();
+            var sha1hash = sha1.ComputeHash(hash);
+            var hashedPassword = Encoding.ASCII.GetString(sha1hash);
+            Properties.Settings.Default.passwd = hashedPassword;
+
+            Properties.Settings.Default.Save();
+        }
+    }
+}
--- Password_Hash_Save_Example/MainWindow.xaml.cs
+++ Password_Hash_Save_Example/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +22,69 @@ namespace Password_Hash_Save_Example
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const string SALT = "HASH_SALT";
+
         public MainWindow()
         {
             InitializeComponent();
+
+            //first start (execute when empty info)
+            if (Properties.Settings.Default.id == null || Properties.Settings.Default.id == "")
+            {
+                //id = testID
+                Properties.Settings.Default.id = "testID";
+
+                //password = test1234
+                var hash = Encoding.ASCII.GetBytes(SALT + "test1234");
+                var sha1 = new SHA1CryptoServiceProvider();
+                var sha1hash = sha1.ComputeHash(hash);
+                var hashedPassword = Encoding.ASCII.GetString(sha1hash);
+                Properties.Settings.Default.passwd = hashedPassword;
+
+                //save
+                Properties.Settings.Default.Save();
+            }
         }
+        
+        //Save button click
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //bring text in Textbox
+            var id = id_TextBox.Text;
+            var passwd = passwd_PasswordBox.Password;
+
+            //checking right value
+            if (CheckID(id) && CheckPassword(SALT,passwd))
+            {
+                MessageBox.Show("Logged in successfully.");
+            }
+            else
+            {
+                var msg = CheckID(id) ? "Wrong password." : "Wrong ID.";
+                MessageBox.Show(msg);
+            }
+        }
+        //Change Password
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ChangePasswordWindow w = new ChangePasswordWindow(SALT);
+            w.ShowDialog();
+            Properties.Settings.Default.Reload();
+        }
+
+        private bool CheckID(string id)
+        {
+            return (Properties.Settings.Default.id == id);
+        }
+        private bool CheckPassword(string salt, string password)
+        {
+            var hash = Encoding.ASCII.GetBytes(salt + password);
+            var sha1 = new SHA1CryptoServiceProvider();
+            var sha1hash = sha1.ComputeHash(hash);
+            var hashedPassword = Encoding.ASCII.GetString(sha1hash);
+
+            return (Properties.Settings.Default.passwd == hashedPassword);
+        }
+
     }
 }
